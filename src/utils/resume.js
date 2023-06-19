7e511942f70c7d01125f487863480180c75a3018
Balu Babu <balub997@gmail.com>
2023-06-20 03:10:46 +0530

    feat: added all my relevant work experience

---
+++ src/utils/resume.js
@@ -0,0 +1,41 @@
+import logoAirbnb from '@/images/logos/airbnb.svg'
+import logoFacebook from '@/images/logos/facebook.svg'
+import logoPlanetaria from '@/images/logos/planetaria.svg'
+import logoStarbucks from '@/images/logos/starbucks.svg'
+import logoHoppscotch from "@/images/logos/hoppscotch.svg"
+import logoWork from "@/images/logos/work.svg"
+import { BriefcaseIcon } from '@/components/BriefcaseIcon'
+
+export const resume = [
+    {
+        company: 'Hoppscotch',
+        title: 'Product Engineer (Backend)',
+        logo: logoWork,
+        start: '2022',
+        end: {
+            label: 'Present',
+            dateTime: new Date().getFullYear(),
+        },
+    },
+    {
+        company: 'Osure Care Pvt Ltd',
+        title: 'Project Lead',
+        logo: logoWork,
+        start: '2020',
+        end: '2022',
+    },
+    {
+        company: "Various Companies",
+        title: 'Freelance Fullstack Developer',
+        logo: logoWork,
+        start: '2109',
+        end: '2022',
+    },
+    {
+        company: 'Principal Engineer',
+        title: 'SMIDER Technology Pvt Ltd',
+        logo: logoWork,
+        start: '2018',
+        end: '2019',
+        },
+]
